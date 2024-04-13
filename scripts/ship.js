be9da12e82f3de9f8b0a7ebf9de083917f0435da
Luke Wheldale <lukewheldale@gmail.com>
2024-04-13 17:15:09 +0930

    typo

--- scripts/ship.js
+++ scripts/ship.js
@@ -1,9 +1,3 @@
-// Begin your app by creating the Ship class/factory (your choice).
-// Your ‘ships’ will be objects that include their length, the number of times they’ve been hit and whether or not they’ve been sunk.
-// REMEMBER you only have to test your object’s public interface. Only methods or properties that are used outside of your ‘ship’ object need unit tests.
-// Ships should have a hit() function that increases the number of ‘hits’ in your ship.
-// isSunk() should be a function that calculates whether a ship is considered sunk based on its length and the number of hits it has received.
-
 class Ship {
 	constructor(length, numOfHitsReceived) {
 		this.length = length;
@@ -15,7 +9,7 @@ class Ship {
 		return (this.numOfHitsReceived += 1);
 	}
 
-	// determine is ship has been sunk or not
+	// determine if ship has been sunk or not
 	isSunk() {
 		if (this.numOfHitsReceived == this.length) {
 			return true;
