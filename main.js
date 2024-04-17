232a2c05840a4d6afc6313570617abbf9e11b9d7
Luke Wheldale <lukewheldale@gmail.com>
2024-04-17 23:53:40 +0930

    check that DOM is linking to main correctly

--- main.js
+++ main.js
@@ -2,5 +2,12 @@ import "./style.css";
 import Ship from "./scripts/Ship";
 import Gameboard from "./scripts/Gameboard";
 import Player from "./scripts/Player";
+import UserInterface from "./scripts/UserInterface";
 
 const ship = new Ship(5, 1);
+
+function loadDom() {
+	UserInterface();
+}
+
+loadDom();
